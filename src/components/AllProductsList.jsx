c56e3f6a073cdf1df89a28e3ed12b54b65750cc0
Narendra G <narendrag@mkcl.org>
2023-09-13 16:11:59 +0530

    allProductList done with fetch data and search the data

--- src/components/AllProductsList.jsx
+++ src/components/AllProductsList.jsx
@@ -1,110 +1,53 @@
-import React, { useState } from "react";
+
+
+import React, { useEffect, useState } from "react";
 import { Table, Input, Button } from "reactstrap";
 import { useNavigate } from "react-router-dom";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore } from "../firebase"; // Import your Firestore configuration
 
 const AllProductsList = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const data = [
-    {
-      id: 1,
-      name: "Notebook",
-      place: "Pune",
-      age: 19,
-      email: "Booked",
-      paid: "Packed",
-      items: "19",
-      total: "19",
-      statusImage: "",
-    },
-    {
-      id: 2,
-      name: "Book",
-      place: "Sangli",
-      age: 15,
-      email: "Unbooked",
-      paid: "Unpaid",
-      items: "15",
-      total: "15",
-    },
-    {
-      id: 3,
-      name: "Eraser",
-      place: "Tasgaon",
-      age: 7,
-      email: "Booked",
-      paid: "Paid",
-      items: "7",
-      total: "7",
-    },
-    {
-      id: 4,
-      name: "Pen",
-      place: "Satara",
-      age: 45,
-      email: "Unbooked",
-      paid: "Unpaid",
-      items: "45",
-      total: "45",
-    },
-    {
-      id: 5,
-      name: "Pencil",
-      place: "Pune",
-      age: 9,
-      email: "Booked",
-      paid: "Paid",
-      items: "9",
-      total: "9",
-    },
-    {
-      id: 6,
-      name: "Bag",
-      place: "Pune",
-      age: 27,
-      email: "Unbooked",
-      paid: "Unpaid",
-      items: "27",
-      total: "27",
-    },
-    {
-      id: 7,
-      name: "Colour Pen",
-      place: "Pune",
-      age: 24,
-      email: "Booked",
-      paid: "Paid",
-      items: "24",
-      total: "24",
-    },
-    {
-      id: 8,
-      name: "Drawing Book",
-      place: "Pune",
-      age: 14,
-      email: "Unbooked",
-      paid: "Unpaid",
-      items: "14",
-      total: "14",
-    },
-    {
-      id: 9,
-      name: "Story Book",
-      place: "Pune",
-      age: 49,
-      email: "Booked",
-      paid: "Paid",
-      items: "49",
-      total: "49",
-    },
-  ];
+  const [products, setProducts] = useState([]); // State variable to store product data
+  const [filteredProducts, setFilteredProducts] = useState([]); // State variable to store filtered product data
+
   const navigate = useNavigate();
 
+  // Fetch product data from Firestore when the component mounts
+  useEffect(() => {
+    const fetchProductData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(firestore, "productt"));
+        const productData = [];
+        querySnapshot.forEach((doc) => {
+          productData.push({ id: doc.id, ...doc.data() });
+        });
+        console.log("fetch data", productData);
+        setProducts(productData);
+        setFilteredProducts(productData); // Initialize filtered products with all products
+      } catch (error) {
+        console.error("Error fetching product data:", error);
+      }
+    };
+
+    fetchProductData();
+  }, []);
+
+  const handleSearchInputChange = (e) => {
+    const searchText = e.target.value;
+    setSearchTerm(searchText);
+
+    // Filter products based on the search term
+    const filteredData = products.filter((item) =>
+      item.productData.productName
+        .toLowerCase()
+        .includes(searchText.toLowerCase())
+    );
+    setFilteredProducts(filteredData);
+  };
   const NavigateToAddProduct = () => {
     navigate("/add-product");
   };
-  const filteredData = data.filter((item) =>
-    item.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <>
@@ -113,7 +56,7 @@ const AllProductsList = () => {
           type="text"
           placeholder="Search by Name"
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleSearchInputChange}
           style={{
             marginTop: "15px",
             marginBottom: "15px",
@@ -145,37 +88,31 @@ const AllProductsList = () => {
           </Button>
         </div>
 
-        {/* <div className="button-container" style={{ marginRight: "10px" }}></div> */}
-        <caption>Orders</caption>
+        <caption>Products</caption>
         <Table>
           <thead>
             <tr>
               <th>#</th>
-              <th>Order No & Name</th>
-              <th>Order Place</th>
-              <th>Order Status</th>
-              <th>Paid</th>
-              <th>Items</th>
-              <th>Total</th>
-              <th>Status</th>
+              <th>Product Name</th>
+              <th>Current Price</th>
+              <th>Product Details</th>
+              <th>Image</th>
             </tr>
           </thead>
+
           <tbody>
-            {filteredData.map((item) => (
+            {filteredProducts.map((item, index) => (
               <tr key={item.id}>
-                <th scope="row">{item.id}</th>
-                <td>{item.name}</td>
-                <td>{item.place}</td>
-                <td>{item.email}</td>
-                <td>{item.paid}</td>
-                <td>{item.items}</td>
-                <td>{item.total}</td>
+                <th scope="row">{index + 1}</th>
+                <td>{item.productData.productName}</td>
+                <td>{item.productData.currentPrice}</td>
+                <td>{item.productData.productDetails}</td>
                 <td>
                   <img
-                    src={item.statusImage} // Use the statusImage property
-                    alt="Status" // Provide an alt text for accessibility
-                    width="50" // Adjust the width as needed
-                    height="50" // Adjust the height as needed
+                    src={item.productData.imageUrl}
+                    alt="Product"
+                    width="50"
+                    height="50"
                   />
                 </td>
               </tr>
@@ -183,15 +120,6 @@ const AllProductsList = () => {
           </tbody>
         </Table>
       </div>
-      {/* <Button
-                  color="primary"
-                  type="button"
-                  href="#"
-                  onClick={(e) => e.preventDefault()}
-                  size="sm"
-                >
-                  Submit
-                </Button> */}
     </>
   );
 };
